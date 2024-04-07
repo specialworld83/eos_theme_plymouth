abbd4e5a26e14f550b4faa04dd19fcdfda32eb41
Team Codelinsoft <specialworld83@gmail.com>
2024-04-07 10:09:59 +0200

    Delete install.sh

--- install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo cp -R ./eos /usr/share/plymouth/themes/
-sudo sed -i "s/mkinitcpio -P/dracut-rebuild/g" /usr/lib/plymouth/plymouth-update-initrd
-sudo plymouth-set-default-theme -R eos
